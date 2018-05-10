54cc064a501adfced60e0be0f0b164aaef9a8cb7
zhaoheqing <332777428@qq.com>
2018-05-10 17:53:14 +0800

    1.2版本

---
+++ mylibrary/src/main/java/com/hsq/mylibrary/A.java
@@ -0,0 +1,20 @@
+package com.hsq.mylibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2018/5/10.
+ */
+
+public class A extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("sfd水电费");
+        setContentView(textView);
+    }
+}
